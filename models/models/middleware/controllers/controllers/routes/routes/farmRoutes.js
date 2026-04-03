877b4496b80ff378455146d0e0f6cadf627841f0
Drishti <your_email@example.com>
2026-04-03 21:38:39 +0530

    Remove server, node_modules - keep only client and docs

--- models/models/middleware/controllers/controllers/routes/routes/farmRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const auth = require("../middleware/authMiddleware");
-const { addFarmData, getFarmData } = require("../controllers/farmController");
-
-router.post("/", auth, addFarmData);
-router.get("/", auth, getFarmData);
-
-module.exports = router;
